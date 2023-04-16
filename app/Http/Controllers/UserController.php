3e802a1e5faddee13b9b4546fafee4049e731a4c
archcos <arjay.charcos25@gmail.com>
2023-04-16 11:52:11 +0800

    MVC2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
-use App\Models\User;
+use App\User;
 use App\Traits\ApiResponser;
 use DB;
 
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,27 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
-
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+Route::get('/', function () {
+    return view('welcome');
 });
- 
-$router->get('/user',['uses' => 'UserController@getUsers']);
+
+Route::get('/user',['uses' => 'UserController@getUsers']);
 
 
-$router->get('/users','UserController@index');
-$router->post('/users','UserController@add');
-$router->get('/users/{id}','UserController@show');
-$router->delete('/users/{id}','UserController@delete');
-$router->put('/users/{id}','UserController@update');
+Route::get('/users','UserController@index');
+Route::post('/users','UserController@add');
+Route::get('/users/{id}','UserController@show');
+Route::delete('/users/{id}','UserController@delete');
+Route::put('/users/{id}','UserController@update');
 
 
