20cfe42c93646a5706bd50140630291e839a2d51
archcos <arjay.charcos25@gmail.com>
2023-04-16 11:38:58 +0800

    MVC

--- app/User.php
+++ app/User.php
@@ -5,35 +5,17 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
-{
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
+class User extends Model{
+   protected $table = 'tblusers';
+// column sa table
+   protected $fillable = [
+    'fullname', 'password', 'email'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+   public $timestamps = false;
+   protected $primaryKey = 'id';
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+   protected $hidden = ['password'];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,29 @@
 <?php
 
+/** @var \Laravel\Lumen\Routing\Router $router */
+
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+$router->get('/', function () use ($router) {
+    return $router->app->version();
 });
+ 
+$router->get('/user',['uses' => 'UserController@getUsers']);
+
+
+$router->get('/users','UserController@index');
+$router->post('/users','UserController@add');
+$router->get('/users/{id}','UserController@show');
+$router->delete('/users/{id}','UserController@delete');
+$router->put('/users/{id}','UserController@update');
+
+
